5575bbf99a02cb9d4c2fa4a0dde8ef8af6cef571
Shanika Lindsay <shanika.m.lindsay@hotmail.co.uk>
2023-08-20 22:11:57 +0100

    Created mock Nike webpage

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero, 
+    CustomerReviews, 
+    PopularProducts, 
+    SuperQuality, 
+    Services, 
+    SpecialOffer, 
+    Subscribe, 
+    Footer
+}
